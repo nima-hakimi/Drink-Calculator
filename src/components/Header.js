8ee917f42c533e55ae1a117f95d911cb22476fea
Nima Hakimi <nima.hakimi@hotmail.com>
2021-05-01 10:45:48 +0200

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Input from './Input'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Drink Calculator</h1>
+            <Input />
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Input.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Input = () => {
+    return (
+        <div className="input">
+            <input type="number" min="0" placeholder="Antall drinks du skal lage" />
+        </div>
+    )
+}
+
+export default Input
